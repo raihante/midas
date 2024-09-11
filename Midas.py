5c27006e6e9af31046d27bc644d355a768258ec4
R E G E X <raehancyber@gmail.com>
2024-09-12 01:46:44 +0700

    add save_token account evading 429

--- Midas.py
+++ Midas.py
@@ -14,8 +14,6 @@ from pyfiglet import Figlet
 from colorama import Fore
 from onlylog import Log
 
-
-
 tix = 0
 
 def banner():
@@ -23,35 +21,93 @@ def banner():
     os.system("cls" if os.name == "nt" else "clear")
     custom_fig = Figlet(font='slant')
     print('')
-    print(custom_fig.renderText(' MIDAS'));
+    print(custom_fig.renderText(' MIDAS'))
     print(Fore.RED + '[#] [C] R E G E X    ' + Fore.GREEN + '[MIDAS BOT] $$ ' + Fore.RESET)
-    print(Fore.GREEN +'[#] Welcome & Enjoy !', Fore.RESET)
-    print(Fore.YELLOW +'[#] Having Troubles? PM Telegram [t.me/fakinsit] ', Fore.RESET)
+    print(Fore.GREEN + '[#] Welcome & Enjoy !', Fore.RESET)
+    print(Fore.YELLOW + '[#] Having Troubles? PM Telegram [t.me/fakinsit]', Fore.RESET)
     print('')
 
-def runforeva(): 
+def runforeva():
     with open('quentod.txt', 'r') as file:
         queryh = file.read().splitlines()
     try:
-            value = True
-            while (value):
-                for index, query_id in enumerate(queryh, start=1):
-                    getname(query_id)
-                    postrequest(gettoken(query_id))
+        value = True
+        while value:
+            for index, query_id in enumerate(queryh, start=1):
+                username = getname(query_id)
+                
+                # Check if username already has a token
+                if has_token(username):
+                    # Get the existing token
+                    token = get_existing_token(username)
+                    
+                    # Check if the token is valid
+                    if not is_token_valid(token):
+                        Log.warn(f'Token for @{username} is expired. Generating a new token...')
+                        token = gettoken(query_id)
+                        if token:
+                            save_token(username, token)
+                else:
+                    # If no token exists, generate a new one
+                    token = gettoken(query_id)
+                    if token:
+                        save_token(username, token)
+                
+                postrequest(token)
     except:
         sleep(10)
-        Log.error('[MAIN] error, restarting')
         runforeva()
 
+def is_token_valid(token):
+    """ Check if the provided token is valid. """
+    bear = 'Bearer ' + token
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0',
+        'Connection': 'keep-alive',
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+        'Authorization': bear
+    }
+    try:
+        url = "https://api-tg-app.midas.app/api/referral/referred-users"
+        req = urllib.request.Request(url, None, header, method='GET')
+        response = urllib.request.urlopen(req)
+        time.sleep(5)
+        if response.getcode() == 200:
+            return True
+        else:
+            return False
+    except Exception as e:
+        Log.error(f'[is_token_valid] Failed to check token validity: {e}')
+        return False
+
+
+def has_token(username):
+    """ Check if the username already has a token saved. """
+    if os.path.exists('account_token.json'):
+        with open('account_token.json', 'r') as f:
+            data = json.load(f)
+        return username in data
+    return False
+
+def get_existing_token(username):
+    """ Retrieve the existing token for the given username. """
+    if os.path.exists('account_token.json'):
+        with open('account_token.json', 'r') as f:
+            data = json.load(f)
+        return data.get(username, None)
+    return None
+
 def gettoken(querybro):
-    header =   {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0',
-            'Connection': 'keep-alive',
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-            }
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0',
+        'Connection': 'keep-alive',
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+    }
     try:
         url = "https://api-tg-app.midas.app/api/auth/register"
-        redrop = json.dumps({"initData":querybro}).encode('utf-8')
+        redrop = json.dumps({"initData": querybro}).encode('utf-8')
         req = urllib.request.Request(url, redrop, header, method='POST')
         response = urllib.request.urlopen(req)
         if response.getcode() != 201:
@@ -63,17 +119,19 @@ def gettoken(querybro):
             Log.success('login success')
             cihuy = response.read().decode('utf-8')
             return cihuy
-    except:
-        Log.error('[gettoken] error restarting, check your query_id / user_id maybe expired')
+    except Exception as e:
+        Log.error(f'[gettoken] Failed to get token : {e}')
+        Log.error(f'[gettoken] Use 1.1.1.1 or another VPN to login first!')
 
 def getuser(querybro):
-    bear = 'Bearer '+querybro
-    header =   {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0',
-            'Connection': 'keep-alive',
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json',
-            'Authorization': bear
-            }
+    bear = 'Bearer ' + querybro
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0',
+        'Connection': 'keep-alive',
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+        'Authorization': bear
+    }
     
     global tix
 
@@ -82,89 +140,97 @@ def getuser(querybro):
         urlvisit = "https://api-tg-app.midas.app/api/user/visited"
         req = urllib.request.Request(url, None, header, method='GET')
         response = urllib.request.urlopen(req).read()
+        time.sleep(3)
         result = json.loads(response.decode('utf-8'))
-        jData=result
-        jsonpoints =  jData['points']
-        jsonsd =  jData['streakDaysCount']
-        jsontix =  jData['tickets']
-        Log.warn('GM points : '+ str(jsonpoints))
-        Log.warn('check-in Day : '+ str(jsonsd))
-        Log.warn('tickets : '+ str(jsontix))
+        jsonpoints = result['points']
+        jsonsd = result['streakDaysCount']
+        jsontix = result['tickets']
+        Log.warn('GM points : ' + str(jsonpoints))
+        Log.warn('check-in Day : ' + str(jsonsd))
+        Log.warn('tickets : ' + str(jsontix))
         tix = jsontix
         try:
             reqv = urllib.request.Request(urlvisit, None, header, method='PATCH')
             urllib.request.urlopen(reqv)
         except:
-            None
-    except:
-        Log.error('[getuser] error restarting, check your query_id / user_id maybe expired')
+            pass
+    except Exception as e:
+        Log.error(f'[getuser] Failed to get user : {e}')
 
 def getcheckin(querybro):
-    bear = 'Bearer '+querybro
-    header =   {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0',
-            'Connection': 'keep-alive',
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json',
-            'Authorization': bear
-        }
-        
+    bear = 'Bearer ' + querybro
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0',
+        'Connection': 'keep-alive',
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+        'Authorization': bear
+    }
+    
     url = "https://api-tg-app.midas.app/api/streak"
     
     try:
-
         req = urllib.request.Request(url, None, header, method='GET')
         response = urllib.request.urlopen(req).read()
         result = json.loads(response.decode('utf-8'))
-        jData=result
-        jsonclaimable =  jData['claimable']
-        if jsonclaimable == True:
+        jsonclaimable = result['claimable']
+        if jsonclaimable:
             try:
                 req = urllib.request.Request(url, None, header, method='POST')
-                response = urllib.request.urlopen(req)
+                urllib.request.urlopen(req)
                 Log.s('check-in success!')
             except:
-                None
-        
-    except:
-        Log.error('[getcheckin] error, restarting')
+                pass
+    except Exception as e:
+        Log.error(f'[getuser] Failed to check-in : {e}')
 
 def getname(querybro):
     try:
         found = re.search('user=([^&]*)', querybro).group(1)
         decodedUserPart = unquote(found)
         userObj = json.loads(decodedUserPart)
-        Log.success('username : @' + userObj['username'])
-    except:
-        Log.error('[decodedUsername] error')
+        username = userObj['username']
+        Log.success('username : @' + username)
+        return username
+    except Exception as e:
+        Log.error(f'[getname] Failed to decode username : {e}')
 
+def save_token(username, token):
+    try:
+        data = {}
+        if os.path.exists('account_token.json'):
+            with open('account_token.json', 'r') as f:
+                data = json.load(f)
+        data[username] = token
+        with open('account_token.json', 'w') as f:
+            json.dump(data, f, indent=4)
+        Log.success(f'Token saved for username: @{username}')
+    except Exception as e:
+        Log.error(f'[save_token] Failed to save token: {e}')
 
 def playgame(tomket):
-    bear = 'Bearer '+tomket
-    header =   {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0',
-            'Connection': 'keep-alive',
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json',
-            'Authorization': bear
+    bear = 'Bearer ' + tomket
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0',
+        'Connection': 'keep-alive',
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+        'Authorization': bear
     }
 
     n = tix
 
-    for i in range(0, n):
+    for _ in range(n):
         try:
             url = "https://api-tg-app.midas.app/api/game/play"
             req = urllib.request.Request(url, None, header, method='POST')
             response = urllib.request.urlopen(req).read()
             result = json.loads(response.decode('utf-8'))
-            jData=result
-            try:
-                jsonreward =  jData['points']
-                Log.success('succes play game, reward : '+ str(jsonreward))
-                sleep(5)
-            except:
-                None
-        except:
-            Log.error('[playgame] failed, restarting')
-
+            jsonreward = result.get('points', 0)
+            Log.success('success play game, reward : ' + str(jsonreward))
+            sleep(5)
+        except Exception as e:
+            Log.error(f'[play_game] Failed to starting game : {e}')
 
 def gettask(tomket):
     bear = 'Bearer '+tomket
@@ -193,7 +259,7 @@ def gettask(tomket):
                     req2 = urllib.request.Request(urlstart, None, header, method='POST')
                     urllib.request.urlopen(req2).read()
                 except:
-                    None
+                    pass
             elif item['state'] == 'CLAIMABLE':
                 list_id = []
                 list_name = []
@@ -210,18 +276,19 @@ def gettask(tomket):
                     if responseclaim.getcode() == 201:
                         Log.success('task ' + anjoy[1] + ' claimed!, rewarded ' + str(anjoy[2]) + ' GM Points')
                 except:
-                    None
+                    pass
 
     except:
-        Log.error('[getTask] failed, restarting')
+        pass
 
 def getreff(tomket):
-    bear = 'Bearer '+tomket
-    header =   {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0',
-            'Connection': 'keep-alive',
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json',
-            'Authorization': bear
+    bear = 'Bearer ' + tomket
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0',
+        'Connection': 'keep-alive',
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+        'Authorization': bear
     }
 
     try:
@@ -230,58 +297,48 @@ def getreff(tomket):
         req = urllib.request.Request(urlreff, None, header, method='GET')
         response = urllib.request.urlopen(req).read()
         result = json.loads(response.decode('utf-8'))
-        jData=result
-        if jData['totalPoints'] > 0:
-            jsoncanClaim =  jData['canClaim']
-            jsontotalPoints =  jData['totalPoints']
-            jsontotalTickets =  jData['totalTickets']
-            Log.success('refferal Points : '+ Fore.RESET + str(jsontotalPoints))
-            Log.success('refferal Tickets : '+ Fore.RESET + str(jsontotalTickets))
-            if jsoncanClaim == True:
-                Log.warn('can claim reff : '+ Fore.GREEN + str(jsoncanClaim))
+        if result['totalPoints'] > 0:
+            jsoncanClaim = result['canClaim']
+            jsontotalPoints = result['totalPoints']
+            jsontotalTickets = result['totalTickets']
+            Log.success('referral Points : ' + Fore.RESET + str(jsontotalPoints))
+            Log.success('referral Tickets : ' + Fore.RESET + str(jsontotalTickets))
+            if jsoncanClaim:
+                Log.warn('can claim ref : ' + Fore.GREEN + str(jsoncanClaim))
                 Log.warn('claiming..')
                 req = urllib.request.Request(urlclaimref, None, header, method='POST')
                 responseclaim = urllib.request.urlopen(req)
                 if responseclaim.getcode() == 201:
-                    Log.success('Refferal rewards claimed successfully')
+                    Log.success('Referral rewards claimed successfully')
             else:
-                Log.warn('can claim reff : '+ Fore.RED + str(jsoncanClaim))
+                Log.warn('can claim ref : ' + Fore.RED + str(jsoncanClaim))
         else:
-                Log.warn('can claim reff : '+ Fore.RED + str(jData['canClaim']))
-    except:
-        Log.error('[getreff] failed, restarting')
+            Log.warn('can claim ref : ' + Fore.RED + str(result['canClaim']))
+    except Exception as e:
+        Log.error(f'[get_reff] Failed to getting refferal : {e}')
 
 def sleep(num):
     for i in range(num):
-        print("wait {} seconds".format(num - i), end='\r')
+        print(f"wait {num - i} seconds", end='\r')
         time.sleep(1)
 
-
 def postrequest(bearer):
-
     try:
-        
         getuser(bearer)
         getcheckin(bearer)
-
         if tix > 0:
             Log.warn('have a game tickets!')
             Log.warn('start playing..')
             playgame(bearer)
-
         getreff(bearer)
         gettask(bearer)
-
     except:
-        Log.error('[postrequest] error restarting')
-        time.sleep(5)
+        sleep(10)
         runforeva()
 
     print('-==========[github.com/raihante/midas]==========-')
     sleep(60)
 
-
-# NYALAIN SENDIRI ABANGKUHH
 if __name__ == "__main__":
     try:
         banner()
